25ff72f850df7018e6de2e6a4dc082ad30ac2249
JacobGsell <jacob.gsell@gmx.de>
2022-12-17 18:09:36 +0100

    asdf

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-netlify';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,14 +10,23 @@ const config = {
 	// for more information about preprocessors
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// if true, will create a Netlify Edge Function rather
+			// than using standard Node-based functions
+			edge: false,
+
+			// if true, will split your app into multiple functions
+			// instead of creating a single one for the entire app.
+			// if `edge` is true, this option cannot be used
+			split: false
+		})
 	},
-	  preprocess: [
-    preprocess({
-      postcss: true,
-    }),
-	vitePreprocess
-  ],
+	preprocess: [
+		preprocess({
+			postcss: true,
+		}),
+		vitePreprocess
+	],
 };
 
 export default config;
